2e64e1b3f739a1688d1035752eeb8df7d99dde0b
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-05-07 15:39:56 +0900

    [add]カウントアプリ

---
+++ nuxt-project/nuxt-vuex-counter/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <Counter></Counter>
+    <hr>
+    <Counter></Counter>
+    <hr>
+    <Counter></Counter>
+    <hr>
+  </div>
+</template>
+
+<script>
+import Counter from '~/components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: block;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
